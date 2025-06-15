1bddb2647dc7d45d22a5e41973831844f28a36e8
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2025-06-15 14:12:12 +0200

    Widok notatek

--- JavaNoteFX/src/main/java/pl/note/Application.java
+++ JavaNoteFX/src/main/java/pl/note/Application.java
@@ -3,6 +3,7 @@ package pl.note;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
@@ -10,8 +11,9 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("views/notes-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Notes!");
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+        stage.setTitle("Notatnik");
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.setScene(scene);
         stage.show();
     }
